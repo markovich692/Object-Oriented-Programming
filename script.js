9482b7a73c37499a7ea80097c90efb0abc065f76
Marc Diallo <mark.devguru@outlook.com>
2025-02-08 23:38:23 -0600

    creates the Person constructor

--- script.js
+++ script.js
@@ -1,29 +1,5 @@
 'use strict';
 
-const obj = {
-  name: 'Marc',
-  age: 32,
-  education: 'Undergraduate',
-};
+const Person = function (firstName, birthYear) {};
 
-Object.entries(obj).forEach(function (cur, i) {
-  const [key, value] = cur;
-  console.log(key + ':', value, i);
-});
-
-const newMap = new Map([
-  ['firstName', 'marc'],
-  ['age', 32],
-  [true, 'work'],
-]);
-
-console.log(newMap.values);
-
-for (const el of [...newMap]) {
-  console.log(el);
-  const [key, value] = el;
-
-  if (key === true) {
-    console.log(`The student ${value}`);
-  }
-}
+new Person('Marc', 1992);
