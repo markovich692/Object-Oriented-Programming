0b074294d3261da27e49cd87454134c2ae7c5692
Marc Diallo <mark.devguru@outlook.com>
2025-02-10 13:50:47 -0600

    creates marc and steve instance from the Person constructor

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {};
+const Person = function (firstName, birthYear) {
+  this.name = firstName;
+  this.birthYear = birthYear;
+  this.greet = function () {
+    return `Hello, my name is ${this.name}`;
+  };
+};
 
-new Person('Marc', 1992);
+const marc = new Person('Marc', 1992);
+const steve = new Person('Steve', 1998);
+
+console.log(marc.greet());
+
+// New {} is created
+// function is called; this={}
+// {} is linked to a prototype
+// function automatically returns {} from the beginning
