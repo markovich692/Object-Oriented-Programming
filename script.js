af14dee25f1cfebb9bf220e4cab1fbd2452200cd
Marc Diallo <mark.devguru@outlook.com>
2025-02-10 15:21:06 -0600

    uses the prototype property to add a method to the construstor prototype object

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
+  //Instance Properties
   this.name = firstName;
   this.birthYear = birthYear;
-  this.greet = function () {
-    return `Hello, my name is ${this.name}`;
-  };
+  //   this.greet = function () {
+  //     return `Hello, my name is ${this.name}`;
+  //   };
 };
 
 const marc = new Person('Marc', 1992);
 const steve = new Person('Steve', 1998);
 
-console.log(marc.greet());
-
 // New {} is created
 // function is called; this={}
 // {} is linked to a prototype
 // function automatically returns {} from the beginning
+
+Person.prototype.greet = function () {
+  console.log('called');
+  return `Hello, my name is ${this.name}`;
+};
+
+console.log(Person.prototype);
